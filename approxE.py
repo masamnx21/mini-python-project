de2c94659d72acdfaa113a0020f8ae3cf01f93b4
Nel M <masamnx21@juniata.edu>
2025-03-11 16:20:17 -0400

    updating output

--- approxE.py
+++ approxE.py
@@ -13,17 +13,15 @@ if __name__=='__main__':
     
     
 	#list of N values to try
-	N = 2
+	N = 1
 	err = 1.00
 	#approximating e and calculating abs. and relative error
 	while err > TOL:
 		approx_e = (1 + 1/N)**N
 		err = abs(math.e - approx_e)/math.e
-		N = N**2
-		outfile.write('N: '+str(N))
-		outfile.write(': approx e: '+str(approx_e))
-		outfile.write(' absolute error: '+str(abs(math.e - approx_e)))
-		outfile.write(' relative error: '+str((abs(math.e - approx_e))/math.e)+'\n')
-#               print(err)
-#		err = abs(math.e - approx_e)/math.e
-		#N = N**2
+		N = N*10
+	outfile.write('N: '+str(N))
+	outfile.write(': approx e: '+str(approx_e))
+	outfile.write(', absolute error: '+str(abs(math.e - approx_e)))
+	outfile.write(', relative error: '+str((abs(math.e - approx_e))/math.e)+'\n')
+
