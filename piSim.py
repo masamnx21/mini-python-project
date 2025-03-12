b95c3f609ca02afceecb00c1aabbe41e13ea74dd
Nel M <masamnx21@juniata.edu>
2025-03-12 00:09:29 -0400

    editing output

--- piSim.py
+++ piSim.py
@@ -13,5 +13,5 @@ if __name__=='__main__':
             if (x*x + y*y) < 1:
                 a = a + 1
         pi_approx = 4*a/N[i]
-        print(str(N[i])+': approx pi: '+str(pi_approx), 'absolute error: '+str(abs(math.pi - pi_approx)), 'relative error: '+str((abs(math.pi - pi_approx))/math.pi))
+        print('for N= '+str(N[i])+': approx pi: '+str(pi_approx)+ ', absolute error: '+str(abs(math.pi - pi_approx))+ ', relative error: '+str((abs(math.pi - pi_approx))/math.pi))
 
